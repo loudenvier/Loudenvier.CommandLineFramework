c8204e7ec3f3763eea78255e21e56f396177a191
Felipe Machado <loudenvier@gmail.com>
2024-09-06 16:59:10 -0300

    Removing binaries mistakenly committed

--- Loudenvier.CommandLineFramework/obj/Debug/netstandard2.0/Loudenvier.CommandLineFramework.AssemblyInfo.cs
+++ Loudenvier.CommandLineFramework/obj/Debug/netstandard2.0/Loudenvier.CommandLineFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loudenvier.CommandLineFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2db858958ebf3ac0d9bafcbdbc51d5a6b7d1a3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb8530be9cf5a21cc116ba7267ddf7518fa82b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loudenvier.CommandLineFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loudenvier.CommandLineFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
